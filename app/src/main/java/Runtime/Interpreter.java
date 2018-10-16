31fda8c8828fecfa2234d9fb3d0d101889d33c2c
johnnyjwaity <johnnywaity@gmail.com>
2018-10-15 19:25:01 -0500

    Started JS Engine

--- app/src/main/java/Runtime/Interpreter.java
+++ app/src/main/java/Runtime/Interpreter.java
@@ -6,6 +6,10 @@ import android.widget.TextView;
 import com.johnnywaity.blocklanguage.MainActivity;
 import com.johnnywaity.blocklanguage.R;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 import Blocks.ElseBlock;
 import Blocks.IfBlock;
 import Blocks.InlineBlock;
@@ -18,42 +22,50 @@ public class Interpreter {
     }
 
     public void run(){
-        long timeStart = System.currentTimeMillis();
-        VariableManager varManager = new VariableManager();
-        final TextView console = MainActivity.sharedInstance.findViewById(R.id.console);
-        console.setVisibility(View.VISIBLE);
-        console.setText("");
-        console.bringToFront();
-        InlineBlock currentBlock = startBlock;
-        InlineBlock lastBlock = null;
-        while(currentBlock.getSnappedView() != null){
-            lastBlock = currentBlock;
-            currentBlock = currentBlock.getSnappedView();
-            if(currentBlock instanceof ElseBlock){
-                System.out.println("Last " + lastBlock.getClass());
-                if(lastBlock instanceof IfBlock){
-                    System.out.println(((IfBlock) lastBlock).getEvaledValue());
-                    if(!((IfBlock) lastBlock).getEvaledValue()){
-                        currentBlock.execute();
-                        System.out.println("Executed");
-                    }
-                }
-            }else{
-                currentBlock.execute();
-            }
+//        ScriptEngine engine = new ScriptEngineManager().getEngineByName("rhino");
+//        try {
+//            engine.eval("print('Hello World');");
+//        } catch (ScriptException e) {
+//            e.printStackTrace();
+//        }
 
-        }
-        timeStart = System.currentTimeMillis() - timeStart;
-        console.setText(console.getText() + "\n\n      Time Ran: " + ((float)timeStart)/1000 + "s");
 
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        MainActivity.sharedInstance.findViewById(R.id.MenuList).bringToFront();
-                        console.setVisibility(View.INVISIBLE);
-                    }
-                },
-                5000);
+//        long timeStart = System.currentTimeMillis();
+//        VariableManager varManager = new VariableManager();
+//        final TextView console = MainActivity.sharedInstance.findViewById(R.id.console);
+//        console.setVisibility(View.VISIBLE);
+//        console.setText("");
+//        console.bringToFront();
+//        InlineBlock currentBlock = startBlock;
+//        InlineBlock lastBlock = null;
+//        while(currentBlock.getSnappedView() != null){
+//            lastBlock = currentBlock;
+//            currentBlock = currentBlock.getSnappedView();
+//            if(currentBlock instanceof ElseBlock){
+//                System.out.println("Last " + lastBlock.getClass());
+//                if(lastBlock instanceof IfBlock){
+//                    System.out.println(((IfBlock) lastBlock).getEvaledValue());
+//                    if(!((IfBlock) lastBlock).getEvaledValue()){
+//                        currentBlock.execute();
+//                        System.out.println("Executed");
+//                    }
+//                }
+//            }else{
+//                currentBlock.execute();
+//            }
+//
+//        }
+//        timeStart = System.currentTimeMillis() - timeStart;
+//        console.setText(console.getText() + "\n\n      Time Ran: " + ((float)timeStart)/1000 + "s");
+//
+//        new android.os.Handler().postDelayed(
+//                new Runnable() {
+//                    public void run() {
+//                        MainActivity.sharedInstance.findViewById(R.id.MenuList).bringToFront();
+//                        console.setVisibility(View.INVISIBLE);
+//                    }
+//                },
+//                5000);
     }
 
 
